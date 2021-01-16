2f36efb6419b292f03defd9a6c047627c9df26e1
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-16 13:03:42 +0800

    Start implementation of barebore structure.

--- COVIDMonitoringSystem.Core/COVIDMonitoringSystem.cs
+++ COVIDMonitoringSystem.Core/COVIDMonitoringSystem.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace COVIDMonitoringSystem.Core
 {
     public class COVIDMonitoringSystem
     {
+        public List<SHNFacility> ShnFacilitiesList { get; }
+        public List<Person> PersonList { get; }
+        public List<BusinessLocation> BusinessLocationList { get; }
+        
         public COVIDMonitoringSystem()
         {
+            ShnFacilitiesList = LoadSHNFacilityData();
+            PersonList = LoadPersonData();
+            BusinessLocationList = LoadBusinessLocationData();
         }
         
-        //TODO: Method to load from Person.csv
+        private List<SHNFacility> LoadSHNFacilityData()
+        {
+            //TODO: LoadSHNFacilityData
+            throw new NotImplementedException();
+        }
         
-        //TODO: Method to load from BusinessLocation.csv
+        private List<Person> LoadPersonData()
+        {
+            //TODO: LoadPersonData
+            throw new NotImplementedException();
+        }
 
-        //TODO: Method to load from Web API
+        private List<BusinessLocation> LoadBusinessLocationData()
+        {
+            //TODO: LoadBusinessLocationData
+            throw new NotImplementedException();
+        }
     }
 }
