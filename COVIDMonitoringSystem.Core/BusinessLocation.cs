d572d610d6f6faeb8d8e2f8d4819e56c4fbe3d80
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-01-18 21:46:37 +0800

    implemented businesslocation, safeentry and tracetogethertoken classes

--- COVIDMonitoringSystem.Core/BusinessLocation.cs
+++ COVIDMonitoringSystem.Core/BusinessLocation.cs
@@ -1,7 +1,32 @@
-﻿namespace COVIDMonitoringSystem.Core
+﻿using System;
+
+namespace COVIDMonitoringSystem.Core
 {
     public class BusinessLocation
     {
-        
+        public string BusinessName { get; set; }
+
+        public string BranchCode { get; set; }
+
+        public int MaximumCapacity { get; set; }
+
+        public int VisitorsNow { get; set; }
+
+        public BusinessLocation(string name, string branch, int capacity)
+        {
+            BusinessName = name;
+            BranchCode = branch;
+            VisitorsNow = capacity;
+        }
+
+        public bool IsFull()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
--- COVIDMonitoringSystem.Core/SafeEntry.cs
+++ COVIDMonitoringSystem.Core/SafeEntry.cs
@@ -1,7 +1,30 @@
-﻿namespace COVIDMonitoringSystem.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace COVIDMonitoringSystem.Core
 {
     public class SafeEntry
     {
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public BusinessLocation Location { get; set; }
+
+        public SafeEntry(DateTime entry, BusinessLocation location)
+        {
+            CheckIn = entry;
+            Location = location;
+        }
         
+        public void PerformCheckOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
