4fbcc9e59d272f2c09e13225c9174927ecfa2aee
hokkaidomelk <72664507+hokkaidomelk@users.noreply.github.com>
2021-01-21 15:16:11 +0800

    Loaded business location data, added menu options for SafeEntry.

---
+++ COVIDMonitoringSystem.ConsoleApp/ConsoleRunner.SafeEntryMgr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COVIDMonitoringSystem.ConsoleApp
+{
+    public partial class ConsoleRunner
+    {
+        private void ManageSafeEntry()
+        {
+            MenusCollection["safe-entry"].RunMenuOption();
+        }
+        private void AssignToken()
+        {
+            Console.WriteLine("Assign Token");
+        }
+
+        private void ViewLocations()
+        {
+            Console.WriteLine("View business locations");
+        }
+
+        private void ChangeCapacity()
+        {
+            Console.WriteLine("Change business location capacity");
+        }
+
+        private void CheckIn()
+        {
+            Console.WriteLine("Check In for SafeEntry");
+        }
+
+        private void CheckOut()
+        {
+            Console.WriteLine("Check Out for SafeEntry");
+        }
+    }
+
+}
