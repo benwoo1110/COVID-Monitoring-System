cf71356da559585488f375db3943437abd0da3f1
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-29 20:38:03 +0800

    Add screen classes for travel entry management.

---
+++ COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/GenerateSHNReportScreen.cs
@@ -0,0 +1,21 @@
+﻿using COVIDMonitoringSystem.ConsoleApp.Display;
+using COVIDMonitoringSystem.ConsoleApp.Display.Elements;
+using COVIDMonitoringSystem.Core;
+
+namespace COVIDMonitoringSystem.ConsoleApp.Screens.TravelEntryMgr
+{
+    public class GenerateSHNReportScreen : CovidScreen
+    {
+        public override string Name => "shnReport";
+        
+        private Input reportDate = new Input("reportDate")
+        {
+            Prompt = "Report Date",
+            BoundingBox = {Top = 4}
+        };
+        
+        public GenerateSHNReportScreen(ConsoleDisplayManager displayManager, COVIDMonitoringManager covidManager) : base(displayManager, covidManager)
+        {
+        }
+    }
+}
---
+++ COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/PaySHNChargesScreen.cs
@@ -0,0 +1,14 @@
+﻿using COVIDMonitoringSystem.ConsoleApp.Display;
+using COVIDMonitoringSystem.Core;
+
+namespace COVIDMonitoringSystem.ConsoleApp.Screens.TravelEntryMgr
+{
+    public class PaySHNChargesScreen : CovidScreen
+    {
+        public override string Name => "paySHNCharges";
+        
+        public PaySHNChargesScreen(ConsoleDisplayManager displayManager, COVIDMonitoringManager covidManager) : base(displayManager, covidManager)
+        {
+        }
+    }
+}
