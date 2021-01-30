51b56150815c8671298d33a49e3bd7583b40b117
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-30 09:24:12 +0800

    Fix merge conflicts.

--- COVIDMonitoringSystem.ConsoleApp/Screens/BuilderScreen.cs
+++ COVIDMonitoringSystem.ConsoleApp/Screens/BuilderScreen.cs
@@ -1,16 +1,12 @@
-<<<<<<< HEAD:COVIDMonitoringSystem.ConsoleApp/Display/BuilderScreen.cs
-﻿//============================================================
+//============================================================
 // Student Number : S10203296, S10205301
 // Student Name   : Benedict Woo, Melvin Kee
 // Module Group   : T06
 //============================================================
 
-namespace COVIDMonitoringSystem.ConsoleApp.Display
-=======
-﻿using COVIDMonitoringSystem.ConsoleApp.Display;
+using COVIDMonitoringSystem.ConsoleApp.Display;
 
 namespace COVIDMonitoringSystem.ConsoleApp.Screens
->>>>>>> 366ddc1a39b8c6cb1e1d60ba8cfb7b4cbeba95c5:COVIDMonitoringSystem.ConsoleApp/Screens/BuilderScreen.cs
 {
     public class BuilderScreen : AbstractScreen
     {
