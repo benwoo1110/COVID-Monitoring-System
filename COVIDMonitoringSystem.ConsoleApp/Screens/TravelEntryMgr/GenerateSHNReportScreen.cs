6e61d80d6d2c1d1d870b58a57e2007ca77a8d7c1
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-30 13:42:27 +0800

    Implement shnReport screen.

--- COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/GenerateSHNReportScreen.cs
+++ COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/GenerateSHNReportScreen.cs
@@ -4,7 +4,9 @@
 // Module Group   : T06
 //============================================================
 
+using System;
 using COVIDMonitoringSystem.ConsoleApp.Display;
+using COVIDMonitoringSystem.ConsoleApp.Display.Attributes;
 using COVIDMonitoringSystem.ConsoleApp.Display.Elements;
 using COVIDMonitoringSystem.Core;
 
@@ -24,9 +26,43 @@ namespace COVIDMonitoringSystem.ConsoleApp.Screens.TravelEntryMgr
             Prompt = "Report Date",
             BoundingBox = {Top = 4}
         };
+        private Button generate = new Button("generate")
+        {
+            Text = "[Generate]",
+            BoundingBox = {Top = 6}
+        };
+        private Label result = new Label("result")
+        {
+            BoundingBox = {Top = 8},
+            ClearOnExit = true
+        };
+
+        private Button openFile = new Button("openFile")
+        {
+            Text = "[Open File]",
+            BoundingBox = {Top = 1},
+            Hidden = true
+        };
         
         public GenerateSHNReportScreen(ConsoleDisplayManager displayManager, COVIDMonitoringManager covidManager) : base(displayManager, covidManager)
         {
+            openFile.BoundingBox.SetRelativeBox(result);
+        }
+
+        [OnClick("generate")]
+        private void OnGenerateReport([Parser("reportDate", "result")] DateTime targetDate)
+        {
+            //TODO: More detailed return for a CSV file generation.
+            if (CovidManager.GenerateSHNStatusReportFile(targetDate))
+            {
+                result.Text = "File has been created.";
+                openFile.Hidden = false;
+                ClearAllInputs();
+                return;
+            }
+
+            result.Text = "There was an error generating the report.";
+            openFile.Hidden = true;
         }
     }
 }
