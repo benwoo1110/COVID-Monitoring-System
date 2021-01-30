01cc307d43ca8afe41f97f4d8b179b61bc7a5ff0
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-01-30 15:14:32 +0800

    Add basic UI elements for PaySHNChargesScreen.

--- COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/PaySHNChargesScreen.cs
+++ COVIDMonitoringSystem.ConsoleApp/Screens/TravelEntryMgr/PaySHNChargesScreen.cs
@@ -5,6 +5,7 @@
 //============================================================
 
 using COVIDMonitoringSystem.ConsoleApp.Display;
+using COVIDMonitoringSystem.ConsoleApp.Display.Elements;
 using COVIDMonitoringSystem.Core;
 
 namespace COVIDMonitoringSystem.ConsoleApp.Screens.TravelEntryMgr
@@ -12,6 +13,25 @@ namespace COVIDMonitoringSystem.ConsoleApp.Screens.TravelEntryMgr
     public class PaySHNChargesScreen : CovidScreen
     {
         public override string Name => "paySHNCharges";
+
+        private Header header = new Header("header")
+        {
+            Text = "Pay SHN Charges"
+        };
+        private Input name = new Input("name")
+        {
+            Prompt = "Name",
+            BoundingBox = {Top = 4}
+        };
+        private Button viewPayment = new Button("viewPayment")
+        {
+            Text = "[View Payment]",
+            BoundingBox = {Top = 6}
+        };
+        private Button paymentInfo = new Button("paymentInfo")
+        {
+            BoundingBox = {Top = 8}
+        };
         
         public PaySHNChargesScreen(ConsoleDisplayManager displayManager, COVIDMonitoringManager covidManager) : base(displayManager, covidManager)
         {
